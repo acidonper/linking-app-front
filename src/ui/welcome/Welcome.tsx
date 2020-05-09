0a8486791039865ffa8084464999141c30dbb471
Asier Cidon <asier.cidon@gmail.com>
2020-05-09 14:41:37 +0200

    Added new welcome message

--- src/ui/welcome/Welcome.tsx
+++ src/ui/welcome/Welcome.tsx
@@ -13,25 +13,30 @@ export const Welcome: React.FunctionComponent<{}> = () => {
       <div className={cx("content")}>
         <div role="WelcomePageMainText" className={cx("content__center")}>
           <h1>Welcome Linking App</h1>
+          <h3>
+            "Linking App is an experimental dating application created from an
+            education or training point of view"
+          </h3>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut
-            elementum est. Maecenas ut tincidunt orci, vitae tristique neque.
+            In contrast to other dating applications, Linking App tries to focus
+            on implementing a stringent matching algorithm and limiting the
+            maximum number of users' concurrent conversations.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut
-            elementum est. Maecenas ut tincidunt orci, vitae tristique neque.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut
-            elementum est. Maecenas ut tincidunt orci, vitae tristique neque
+            Why should the maximum number of users' concurrent conversations be
+            limited?
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut
-            elementum est. Maecenas ut tincidunt orci, vitae tristique neque.
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut
-            elementum est. Maecenas ut tincidunt orci, vitae tristique neque
+            In my honest opinion, We have tons of possibilities (supply and
+            demand) in this digital world. There are many single people which
+            are looking for a new opportunity to meet a new person. When you
+            have too many open opportunities, it is so difficult to focus on
+            someone and be able to discover this amazing person.
           </p>
           <p>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In ut
-            elementum est. Maecenas ut tincidunt orci, vitae tristique neque.
+            For this reason, I really think that it is a good starting point to
+            give Linking App's users the possibility of having a real
+            opportunity to meet their matches.
           </p>
         </div>
       </div>
